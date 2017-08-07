1137928f1c46be3dde13470d9d29ab03324c8fe2
danny <dledwith2@hotmail.com>
2017-08-07 05:38:55 -0400

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter a key to exit");
+            Console.ReadKey();
+            
+        }
+    }
+}
